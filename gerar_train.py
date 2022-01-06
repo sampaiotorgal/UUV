b0a7030e9294e4dfcca9d972fbd2513cc8a62a45
sampaiotorgal <95769809+sampaiotorgal@users.noreply.github.com>
2022-01-06 15:50:27 +0000

    Update gerar_train.py

--- gerar_train.py
+++ gerar_train.py
@@ -2,13 +2,13 @@ import os
 
 # o que o codigo abaixo faz: adiciona o nome e caminho de todos as fotos (.jpg) em um txt
 imagens = []
-os.chdir(os.path.join("data", "valid"))
+os.chdir(os.path.join("data", "obj"))
 for filename in os.listdir(os.getcwd()):
     if filename.endswith(".jpg"):
-        imagens.append("data/valid/" + filename)
+        imagens.append("data/obj/" + filename)
 os.chdir("..")
 
-with open("test.txt", "w") as outfile:
+with open("train.txt", "w") as outfile:
     for img in imagens:
         outfile.write(img)
         outfile.write("\n")
